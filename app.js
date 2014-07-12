3160d9c49059ce34276911d44398dad731157902
Joshua Pham <joshua.pham@gmail.com>
2014-07-12 01:45:38 -0700

    began work on actual backend, aside from testing. Support for buffer object list

--- app.js
+++ app.js
@@ -1,49 +1,16 @@
-//BUFFERLOADER CLASS
-function BufferLoader(context, url, callback) {
-  this.context = context;
-  this.url = url;
-  this.onload = callback;
-  this.buffer; //this.bufferList = new Array();
-  this.loadCount = 0;
+function BufferLoader(context, soundList, callback) {
+    this.context = context;
+    this.soundList = soundList;
+    this.onload = callback;
+    this.bufferList = new Array();
+    this.loadCount = 0;
 }
 
-BufferLoader.prototype.loadBuffer = function() {
-    // Load buffer asynchronously
-    var loader = this;
-
-    var request = new XMLHttpRequest();
-    request.open("GET", loader.url, true);
-    request.responseType = "arraybuffer";
-
-    request.onload = function() {
-    // Asynchronously decode the audio file data in request.response
-    loader.context.decodeAudioData(
-        request.response,
-        function(buffer) {
-            if (!buffer) {
-                alert('error decoding file data: ' + url);
-                return;
-            }
-            loader.buffer = buffer;
-            loader.onload(loader.buffer);
-        },
-        function(error) {
-            console.error('decodeAudioData error', error);
-        }
-    );
-    }
-
-    request.onerror = function() {
-        alert('BufferLoader: XHR error');
-    }
-
-    request.send();
-}
-
-/*BufferLoader.prototype.loadBuffer = function(url, index) {
+// loadBuffer also loads sound metadata into the bufferList array.
+BufferLoader.prototype.loadBuffer = function(sound, index) {
     // Load buffer asynchronously
     var request = new XMLHttpRequest();
-    request.open("GET", url, true);
+    request.open("GET", sound.url, true);
     request.responseType = "arraybuffer";
 
     var loader = this;
@@ -57,8 +24,10 @@ BufferLoader.prototype.loadBuffer = function() {
             alert('error decoding file data: ' + url);
             return;
         }
-        loader.bufferList[index] = buffer;
-        if (++loader.loadCount == loader.urlList.length)
+        // buffer is the main purpose, but metadata offset and url are also part of the object.
+        loader.bufferList.push({"buffer": buffer, "offset": sound.offset, "url": sound.url});
+        console.log('loaded buffer ' + index + ": " + sound.url);
+        if (++loader.loadCount == loader.soundList.length)
             loader.onload(loader.bufferList);
         },
         function(error) {
@@ -75,27 +44,65 @@ BufferLoader.prototype.loadBuffer = function() {
 }
 
 BufferLoader.prototype.load = function() {
-    for (var i = 0; i < this.urlList.length; ++i)
-        this.loadBuffer(this.urlList[i], i);
-}*/
-/*~~~~~~~~~~*/
+    for (var i = 0; i < this.soundList.length; ++i) {
+        this.loadBuffer(this.soundList[i], i);
+    }
+}
+
+
+
+function AudioHandler(bufferList, context) {
+    this.recent_start;
+    this.recent_pause;
+    this.bufferList = bufferList;
+    this.source;
+    this.context = context;
+    this.Player = Player(bufferList.length);
+}
+
+AudioHandler.prototype.playManager = function(bufferList, buffer_index, offset_percentage) { //index is number - 1
+    // solely for iteration through buffers.
+    if (buffer_index >= bufferList.length)
+        console.log('end of playlist reached!'); // and then exit funciton....
+
+    while (buffer_index < bufferList.length) {
+        this.play(bufferList[buffer_index], buffer_index, offset_percentage);
+    }
+    
+}
+
+AudioHandler.prototype.play = function(buffer, index, offset_percentage) {
+    // call to refresh correct div, id'd by index.
+    var source = this.source;
+
+    source = this.context.createBufferSource();
+    source.buffer = buffer;
+    source.connect(this.context.destination);
+
+    source.start(0, offset_percentage * source.buffer.duration % source.buffer.duration);
+
+    this.Player.playing = true;
+    
+    this.Player.render(index, offset_percentage);
+}
+
+function Player(numBuffers) {
+    this.numBuffers = numBuffers;
+    this.playing = false;
+
+    //construct div elements here.
+}
+
+Player.prototype.render = function(index, offset_percentage) {
+    if (playing) {
+
+    }
+}
 
 
 window.onload = init;
 
-var context;
-var bufferLoader;
-var source;
-var buffer;
-var last_start = 0; // from beginning of sound
-var start_offset = 0; // from beginning of sound
-
-var divWidth = 600;
-
-var updatedWidth;
-
-var playing = false;
-
+var context, bufferloader;
 
 function init() {
   // Fix up prefixing
@@ -104,68 +111,19 @@ function init() {
     context = new AudioContext();
     bufferloader = new BufferLoader(
         context,
-        "Hard To Smile.mp3",
+        [
+            {"url": "Hard To Smile.mp3", "offset": 5},
+            {"url": "Misterwives - Reflections (Flaxo Remix).mp3", "offset": 10},
+            {"url": "YesYou ft. Marcus Azon - Frivolous Life (Vlad Lucan Remix}.mp3", "offset": 8}
+        ],
         finishedLoading
         );
 
-    bufferloader.loadBuffer();
-
-    function finishedLoading(retrieved_buffer) {
-        buffer = retrieved_buffer;
+    bufferloader.load();
+    console.log(bufferloader);
+    function finishedLoading(retrieved_bufferList) {
+        //bufferList = retrieved_bufferList;
+        console.log('loaded!');
+        //call AudioManager to construct
     }
-
-    function play(offset) {
-        if (typeof(offset) === 'undefined') offset = start_offset;
-        else start_offset = offset;
-
-        playing = true;
-
-        last_start = context.currentTime;
-        source = context.createBufferSource();
-        source.buffer = buffer; //remind from snapshot, or redundant from init.
-        source.connect(context.destination);
-        source.start(0, offset % buffer.duration);
-
-        refreshWidth(offset);
-    }
-
-    function pause() {
-        playing = false;
-        buffer = source.buffer; //snapshot
-
-        source.stop();
-        start_offset += context.currentTime - last_start;
-    }
-
-    function refreshWidth(offset) { //offset is wrap of start_offset
-        if (playing) {
-            if (typeof(offset) === 'undefined') offset = start_offset; // if resuming from straight pause-play
-
-            soundProgress = context.currentTime - last_start + offset;
-            soundPercent = (soundProgress / buffer.duration) * 100;
-            //for animation
-            $('.progress').width(soundPercent + "%");
-            
-            //and refresh number text
-            $('.textProgress').html(soundProgress.toFixed(2) + " / " + buffer.duration.toFixed() + " secs.");
-            window.requestAnimationFrame(function() {
-                refreshWidth(offset);
-            });
-        }
-    }
-
-    $('#play_toggle').click(function() {
-        if (!playing) play();
-        else pause();
-    });
-
-    $('.player').click(function(e) {
-        //x position relative to the top-left of element clicked.
-        var newXpos = (e.clientX - $(this).offset().left);
-        //percentage of progres
-        var percentProgress = newXpos / divWidth;
-
-        pause();
-        play(percentProgress * buffer.duration);
-    });
 }
---
+++ app2.js
@@ -0,0 +1,178 @@
+//BUFFERLOADER CLASS
+function BufferLoader(context, url, callback) {
+  this.context = context;
+  this.url = url;
+  this.onload = callback;
+  this.buffer; //this.bufferList = new Array();
+  this.loadCount = 0;
+}
+
+BufferLoader.prototype.loadBuffer = function() {
+    // Load buffer asynchronously
+    var loader = this;
+
+    var request = new XMLHttpRequest();
+    request.open("GET", loader.url, true);
+    request.responseType = "arraybuffer";
+
+    request.onload = function() {
+    // Asynchronously decode the audio file data in request.response
+    loader.context.decodeAudioData(
+        request.response,
+        function(buffer) {
+            if (!buffer) {
+                alert('error decoding file data: ' + url);
+                return;
+            }
+            loader.buffer = buffer;
+            loader.onload(loader.buffer);
+        },
+        function(error) {
+            console.error('decodeAudioData error', error);
+        }
+    );
+    }
+
+    request.onerror = function() {
+        alert('BufferLoader: XHR error');
+    }
+
+    request.send();
+}
+
+/*BufferLoader.prototype.loadBuffer = function(url, index) {
+    // Load buffer asynchronously
+    var request = new XMLHttpRequest();
+    request.open("GET", url, true);
+    request.responseType = "arraybuffer";
+
+    var loader = this;
+
+    request.onload = function() {
+    // Asynchronously decode the audio file data in request.response
+    loader.context.decodeAudioData(
+        request.response,
+        function(buffer) {
+        if (!buffer) {
+            alert('error decoding file data: ' + url);
+            return;
+        }
+        loader.bufferList[index] = buffer;
+        if (++loader.loadCount == loader.urlList.length)
+            loader.onload(loader.bufferList);
+        },
+        function(error) {
+            console.error('decodeAudioData error', error);
+        }
+    );
+    }
+
+    request.onerror = function() {
+        alert('BufferLoader: XHR error');
+    }
+
+    request.send();
+}
+
+BufferLoader.prototype.load = function() {
+    for (var i = 0; i < this.urlList.length; ++i)
+        this.loadBuffer(this.urlList[i], i);
+}*/
+/*~~~~~~~~~~*/
+
+
+window.onload = init;
+
+var context;
+var bufferLoader;
+var source;
+var buffer;
+var last_start = 0; // from beginning of sound
+var start_offset = 0; // from beginning of sound
+
+var divWidth = 600;
+
+var updatedWidth;
+
+var playing = false;
+
+
+function init() {
+  // Fix up prefixing
+    window.AudioContext = window.AudioContext || window.webkitAudioContext;
+
+    context = new AudioContext();
+    bufferloader = new BufferLoader(
+        context,
+        "Hard To Smile.mp3",
+        finishedLoading
+        );
+
+    bufferloader.loadBuffer();
+
+    function finishedLoading(retrieved_buffer) {
+        buffer = retrieved_buffer;
+        console.log('loaded!');
+    }
+
+    function play(offset) {
+        if (typeof(offset) === 'undefined') offset = start_offset;
+        else start_offset = offset;
+
+        playing = true;
+
+        last_start = context.currentTime;
+        source = context.createBufferSource();
+        source.buffer = buffer; //remind from snapshot, or redundant from init.
+        source.connect(context.destination);
+        source.start(0, offset % buffer.duration);
+
+        refreshWidth(offset);
+
+        source.onended = function() {
+            console.log('finished');
+            source.stop();
+            playing = false;
+        }
+    }
+
+    function pause() {
+        playing = false;
+        buffer = source.buffer; //snapshot
+
+        source.stop();
+        start_offset += context.currentTime - last_start;
+    }
+
+    function refreshWidth(offset) { //offset is wrap of start_offset
+        if (playing) {
+            if (typeof(offset) === 'undefined') offset = start_offset; // if resuming from straight pause-play
+
+            soundProgress = context.currentTime - last_start + offset;
+            soundPercent = (soundProgress / buffer.duration) * 100;
+            //for animation
+            $('.progress').width(soundPercent + "%");
+            
+            //and refresh number text
+            $('.textProgress').html(soundProgress.toFixed(2) + " / " + buffer.duration.toFixed() + " secs.");
+            window.requestAnimationFrame(function() {
+                refreshWidth(offset);
+            });
+        }
+    }
+
+    $('#play_toggle').click(function() {
+        if (!playing) play();
+        else pause();
+    });
+
+    $('.player').click(function(e) {
+        //x position relative to the top-left of element clicked.
+        var newXpos = (e.clientX - $(this).offset().left);
+        //percentage of progres
+        var percentProgress = newXpos / divWidth;
+
+        if (playing) pause();
+        play(percentProgress * buffer.duration);
+    });
+}
